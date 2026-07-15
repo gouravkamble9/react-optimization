745cafca4bede28c1791d270f9bf4ca8f84391f3
gouravkamble9 <gouravkamble9@gmail.com>
2026-07-15 11:07:58 +0530

    implement usememo hook

--- src/memo/Memo.jsx
+++ src/memo/Memo.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 
 const Memo = () => {
-  return (
-    <div>Memo</div>
-  )
+    const [count, setCount] = useState(0)
+    const [text, setText] = useState("")
+
+    const expensiveCalculation = (num) => {
+        console.log("Calculating...")
+        for (let i = 0; i < 10000000; i++) { }
+        return num * 2
+    }
+
+    const result = useMemo(() => {
+        return expensiveCalculation(count)
+    }, [count])
+
+    return (
+        <div>
+            <h2>Result: {result}</h2>
+
+            <button onClick={() => setCount(count + 1)}>
+                Increment Count
+            </button>
+
+            <input
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                placeholder="Type here"
+            />
+        </div>
+    )
 }
 
 export default Memo
